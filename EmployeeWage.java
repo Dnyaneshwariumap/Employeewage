130a16da1b95866a6f33234a8477e879106b31c2
Dnyaneshwari <dnyaneshwariumap994@gmail.com>
2022-10-03 23:30:22 +0530

    Added: Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,15 @@
 package emp;
-
+import java.util.Random;
 public class EmployeeWage {
-    public static void main(String[]args){
-        System.out.println("Welcome employee wage problem" );
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation");
+        Random r = new Random();//UC1
+        int empcheck = r.nextInt(2) + 1;
+        if (empcheck == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee Is Absent");
+        }
     }
 }
